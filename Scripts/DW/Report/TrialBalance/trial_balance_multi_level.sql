471ed3028fb1b225514260c129454c96e9bea2ac
brentgroves <brent.groves@gmail.com>
2024-02-27 19:20:15 -0500

    updated source code

--- Scripts/DW/Report/TrialBalance/trial_balance_multi_level.sql
+++ Scripts/DW/Report/TrialBalance/trial_balance_multi_level.sql
@@ -1,3 +1,224 @@
+/*
+ * 00
+ */
+
+-- Plex.Cost_Gross_Margin_Daily_View source
+select top 10 * from Plex.Cost_Gross_Margin_Daily_View
+-- drop view Plex.Cost_Gross_Margin_Daily_View
+-- drop view Plex.price_list
+-- drop view Plex.account_period_low
+-- drop view Plex.accounting_account123
+-- drop view Plex.accounting_period_low_view
+-- drop view Report.daily_metrics_pivot_view
+-- drop view Scratch.accounting_period_balance_low
+-- drop view Plex.daily_shift_report_data_daily_metrics_criteria_view
+create view Plex.Cost_Gross_Margin_Daily_View
+as
+with all_po
+as 
+(
+	select gm.*
+	--select count(*)
+	from Plex.Cost_Gross_Margin_View gm 
+),
+--select * from all_po 
+--where valid !=0
+part_aggregate  
+as 
+( 
+	select ap.pcn,ap.Plexus_Customer_Code,ap.report_date,ap.Part_No,ap.revision,
+	sum(ap.sales_qty) shipped,
+	sum(ap.sales_qty*ap.unit_price) total_sales,  -- see validation tab of daily_metrics validation spreadsheet.
+	sum(ap.sales_qty*ap.unit_price) --total_sales,  -- see validation tab of daily_metrics validation spreadsheet.
+	/
+	sum(ap.sales_qty) -- shipped,
+	sell_price,		
+	count(distinct Unit_Price) price_count,
+	count(*) po_count,
+	min(Unit_Price) min_price,
+	max(Unit_Price) max_price,
+	max(ap.valid) max_valid -- most important issue.
+	from all_po ap 
+	group by ap.pcn,ap.Plexus_Customer_Code,ap.report_date,ap.Part_No,ap.revision
+
+),
+-- select * from part_aggregate 
+price_diff 
+as 
+( 
+	select *
+	--select count(*) 
+	from part_aggregate  
+	where max_price - min_price > .01
+),
+-- select * from price_diff 
+--select count(*) 
+--from price_diff 
+po_price_diff 
+as 
+(
+	select 
+	ap.* 
+	from all_po ap 
+	inner join price_diff pd
+	on ap.pcn = pd.pcn 
+	and ap.report_date = pd.report_date 
+	and ap.part_no = pd.part_no 
+	and ap.revision = pd.revision 
+	
+),
+-- select * from po_price_diff 
+price_list
+as 
+(
+	select main.pcn,main.Plexus_Customer_Code,main.report_date,main.part_no,main.revision,
+	left(main.price_list,len(main.price_list)-1) as price_list 
+	from 
+	(
+	
+		select distinct pd2.pcn,pd2.Plexus_Customer_Code,pd2.report_date,pd2.part_no,pd2.revision, 
+			(
+				select 
+				case 
+				when pd1.po_no = '' and pd1.unit_price is null then 'no-po/no-price;'
+				when pd1.po_no = '' and pd1.unit_price is not null then 'no-po/' + cast(pd1.unit_price as varchar) + ';'
+				when pd1.po_no != '' and pd1.unit_price is null then pd1.po_no + '/no-price;'
+				else pd1.po_no + '/' + cast(pd1.unit_price as varchar) + ';'
+				end as [text()]
+				from po_price_diff pd1 
+				where pd1.pcn = pd2.pcn 
+				and pd1.report_date = pd2.report_date 
+				and pd1.part_no = pd2.part_no 
+				and pd1.revision = pd2.revision 
+				order by pd1.pcn,pd1.report_date,pd1.part_no,pd1.revision 
+				for xml path (''), type 
+			).value('text()[1]','varchar(max)') [price_list]
+		from po_price_diff pd2 
+	) [main]	
+)
+-- select * from price_list 
+select pa.pcn,pa.plexus_customer_code,pa.report_date,pa.part_no,pa.revision,
+shipped,
+sell_price,
+total_sales, 
+case 
+when pl.price_list is null then ''
+else pl.price_list 
+end price_list,
+pa.max_valid valid
+from part_aggregate pa  
+left outer join price_list pl 
+on pa.pcn = pl.pcn 
+and pa.report_date = pl.report_date 
+and pa.part_no = pl.part_no 
+and pa.revision = pl.revision;
+
+-- Plex.price_list source
+	select top 10 *
+	from Plex.Cost_Gross_Margin_Daily_View gm
+
+
+create view Plex.price_list 
+as 
+with all_po
+as 
+(
+	select pcn,Plexus_Customer_Code,report_date,Part_No,revision,sell_price,gm.PO_No
+	from Plex.Cost_Gross_Margin_Daily_View gm
+),
+--select * from all_po 
+part_aggregate  
+as 
+( 
+	select ap.pcn,ap.Plexus_Customer_Code,ap.report_date,ap.Part_No,ap.revision,
+	count(distinct Unit_Price) price_count,
+	count(*) po_count,
+	min(Unit_Price) min_price,
+	max(Unit_Price) max_price
+	from all_po ap 
+	group by ap.pcn,ap.Plexus_Customer_Code,ap.report_date,ap.Part_No,ap.revision
+
+),
+price_diff 
+as 
+( 
+	select *
+	--select count(*) 
+	from part_aggregate  
+	where max_price - min_price > .01
+),
+--select *
+--select count(*) 
+--from price_diff 
+po_price_diff 
+as 
+(
+	select ap.* 
+	from all_po ap 
+	inner join price_diff pd
+	on ap.pcn = pd.pcn 
+	and ap.report_date = pd.report_date 
+	and ap.part_no = pd.part_no 
+	and ap.revision = pd.revision 
+	
+),
+/*
+			select 
+			case 
+			when pd1.po_no = '' and pd1.unit_price is null then 'no-po/no-price;'
+			when pd1.po_no = '' and pd1.unit_price is not null then 'no-po/' + cast(pd1.unit_price as varchar) + ';'
+			when pd1.po_no != '' and pd1.unit_price is null then pd1.po_no + '/no-price;'
+			else pd1.po_no + '/' + cast(pd1.unit_price as varchar) + ';'
+			end as [text()]
+			from po_price_diff pd1 
+*/		
+/*
+	select distinct pd2.pcn,pd2.report_date,pd2.part_no,pd2.revision 
+	from po_price_diff pd2 	
+*/
+/*
+	select 
+		(
+			select 
+			case 
+			when pd1.po_no = '' then 'no-po,'
+			else pd1.po_no + ',' 
+			end as [text()]
+			from po_price_diff pd1 
+			order by pd1.pcn,pd1.report_date,pd1.part_no,pd1.revision 
+			for xml path (''), type 
+		).value('text()[1]','varchar(max)') [prices]
+*/
+price_list
+as 
+(
+	select main.Plexus_Customer_Code,main.report_date,main.part_no,main.revision,
+	left(main.prices,len(main.prices)-1) as prices 
+	from 
+	(
+	
+		select distinct pd2.pcn,pd2.Plexus_Customer_Code,pd2.report_date,pd2.part_no,pd2.revision, 
+			(
+				select 
+				case 
+				when pd1.po_no = '' and pd1.unit_price is null then 'no-po/no-price;'
+				when pd1.po_no = '' and pd1.unit_price is not null then 'no-po/' + cast(pd1.unit_price as varchar) + ';'
+				when pd1.po_no != '' and pd1.unit_price is null then pd1.po_no + '/no-price;'
+				else pd1.po_no + '/' + cast(pd1.unit_price as varchar) + ';'
+				end as [text()]
+				from po_price_diff pd1 
+				where pd1.pcn = pd2.pcn 
+				and pd1.report_date = pd2.report_date 
+				and pd1.part_no = pd2.part_no 
+				and pd1.revision = pd2.revision 
+				order by pd1.pcn,pd1.report_date,pd1.part_no,pd1.revision 
+				for xml path (''), type 
+			).value('text()[1]','varchar(max)') [prices]
+		from po_price_diff pd2 
+	) [main]	
+)
+select * from price_list;
+
 Period,Category Type,Category Name,Sub Category Name,No,Name,Current Debit/(Credit),YTD Debit/(Credit)
 "10-2021","Asset","Current Assets","Receivables","10220-000-00000","Accounts Receivable","0","6353429.89999998",
 -- drop table Plex.trial_balance_multi_level
